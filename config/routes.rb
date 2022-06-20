0acd35eb007772b59c41363348435ed29c0f11a4
koske0125 <kousuke.0125.dm@gmail.com>
2022-06-20 07:02:59 +0000

    [Add]ヘッダーの調整

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,15 @@ devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
+
+# get '/customers/is_deleted' => 'public/customers#is_deleted'
+  namespace :public, path: "" do
+    get '/' => 'homes#top'
+
+  end
+
 end
